f64968b63ce5f4769f2b36f3efa6ec1e5cff3dd2
monishmv <monish.kumar@spoonshot.com>
2022-07-12 17:27:19 +0530

    Updates

---
+++ precache-manifest.3f5008beaa94614c52c147ac80c22a2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3149057e1bd34dd9c3bd8794800df974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e953add692f748b3f6",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e953add692f748b3f6",
+    "url": "/static/js/main.007f62ef.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
