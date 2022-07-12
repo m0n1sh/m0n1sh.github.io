986dc8594acff5d2f204efdd02e64bbc7eb318c8
monishmv <monish.kumar@spoonshot.com>
2022-07-13 00:59:26 +0530

    Updates

---
+++ precache-manifest.13eb406ce6bcd4de34a5a769a38ee0e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cb6e94c009e6736cfd6157a64299cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a38754d2e5f9cf5ec46f",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38754d2e5f9cf5ec46f",
+    "url": "/static/js/main.fab87893.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "99388abc00a7f24e42cede9db64ec009",
+    "url": "/static/media/boeing.99388abc.png"
+  },
+  {
+    "revision": "43e40b232690cf7c2894feafffee92af",
+    "url": "/static/media/dell.43e40b23.png"
+  },
+  {
+    "revision": "1104df6f501c741b51b8afcbd125879b",
+    "url": "/static/media/hashirshoaeb.1104df6f.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.c5e7931ed018e21efe3928b49513138d.js"
+  "/precache-manifest.13eb406ce6bcd4de34a5a769a38ee0e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
